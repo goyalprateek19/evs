659de63e8622a50ad97934c45f5409336b4f0bf9
goyalprateek19 <69925414+goyalprateek19@users.noreply.github.com>
2022-09-02 11:36:35 +0530

    Add files via upload

---
+++ email-verification-service/src/main/java/com/hdfc/emailverificationservice/controller/EVSController.java
@@ -0,0 +1,14 @@
+package com.hdfc.emailverificationservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EVSController {
+	
+	@RequestMapping(value = "/custom", method = RequestMethod.POST)
+	public String custom() {
+		return "custom";
+	}
+}
